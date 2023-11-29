d91b6e7e5e79ad47b7b32991b3ac465aa2888cb2
Ben Neiman <bensneiman@gmail.com>
2023-11-28 23:04:21 -0800

    install mangum

--- anet_api/main.py
+++ anet_api/main.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI
 
 from anet_api.routers import team, meet, athlete, search
 
+from mangum import Mangum
+
 app = FastAPI()
 
 app.include_router(team.router)
 app.include_router(meet.router)
 app.include_router(athlete.router)
 app.include_router(search.router)
+
+handler = Mangum(app)
 
 
 @app.get("/")
