6ee6403460b8b5304d4362a4ffbf10c4c083eaac
Ben Neiman <bensneiman@gmail.com>
2024-12-19 12:04:52 -0800

    abstract db url to env

--- anet_api/db/database.py
+++ anet_api/db/database.py
@@ -1,9 +1,9 @@
 from sqlmodel import Session, create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLMODEL_DATABASE_URL = "postgresql+psycopg2:///anet_results"
+from settings import DB_URL
 
-engine = create_engine(SQLMODEL_DATABASE_URL)
+engine = create_engine(DB_URL)
 
 SessionLocal = sessionmaker(engine, class_=Session)
 
--- anet_api/db/migrations/env.py
+++ anet_api/db/migrations/env.py
@@ -7,11 +7,13 @@ from alembic import context
 
 from sqlmodel import SQLModel
 
-from models import Athlete, Team, Meet, Result
+from settings import DB_URL
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("DB_URL", DB_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
