59c8178fc88cf2871372cd76b517d2f0bbfc05af
Ben Neiman <bensneiman@gmail.com>
2024-12-20 17:51:47 -0800

    sneak in another test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,12 +4,18 @@ from pydantic import ValidationError
 from fastapi.testclient import TestClient
 from sqlmodel import Session
 
-from anet_api.db.utils import create_item, get_object_by_anet_id, convert_to_seconds
+from anet_api.db.utils import (
+    create_item,
+    get_object_by_anet_id,
+    get_course_by_venue,
+    convert_to_seconds,
+)
 
-from anet_api.db import Team, TeamCreate, Athlete, AthleteCreate
-from anet_api.constants import DB_PREFIX, POST_TEAM
+from anet_api.db import Team, TeamCreate, Athlete, AthleteCreate, Course
+from anet_api.constants import DB_PREFIX, POST_TEAM, POST_COURSE
 
 post_team_endpoint = DB_PREFIX + POST_TEAM
+post_course_endpoint = DB_PREFIX + POST_COURSE
 
 
 def test_convert_to_seconds():
@@ -44,3 +50,12 @@ def test_create_item_invalid(session: Session, client: TestClient):
     team = TeamCreate(anet_id=1, name="Test Team")
     with pytest.raises(ValidationError) as e_info:
         create_item(session, team, Athlete)
+
+
+def test_get_course_by_venue(session: Session, client: TestClient):
+    response = client.post(
+        post_course_endpoint, json={"venue": "fake park", "course_factor": 100}
+    )
+
+    res = get_course_by_venue(session, "fake park")
+    assert isinstance(res, Course)
